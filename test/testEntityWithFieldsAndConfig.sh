240985640d2d7acabdf1eb6d83ce75d56d628d64
Yann <yann.barraud@gmail.com>
2013-09-18 10:57:13 +0200

    Added indus-mode
    New indus-mode
    Travis config updated (no more auto upload to bintray)
    Still on 0.90.3
    Still needs refactoring & comments... but runs

---
+++ test/testEntityWithFieldsAndConfig.sh
@@ -0,0 +1,105 @@
+./testInit.sh
+echo
+curl -XPUT "http://localhost:9200/entity/" -d '{
+  "settings": {
+    "index.number_of_shards": 1,
+    "index.number_of_replicas": 0
+  },
+  "mappings": {
+    "entity-configuration": {
+      "properties": {
+        "name": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "comparator": {
+          "type": "string",
+          "index": "not_analyzed"
+        },
+        "low": {
+          "type": "double"
+        },
+        "high": {
+          "type": "double"
+        },
+        "cleaners": {
+            "type": "string",
+            "index" : "not_analyzed"
+        }
+      }
+    }
+  }
+}'
+echo
+curl -XPUT "localhost:9200/entity/entity-configuration/test" -d '{
+  "entity": {
+    "fields": [
+      {
+        "field": "city",
+        "cleaners": [
+          "no.priv.garshol.duke.cleaners.TrimCleaner",
+          "no.priv.garshol.duke.cleaners.LowerCaseNormalizeCleaner"
+        ],
+        "comparator": "no.priv.garshol.duke.comparators.Levenshtein",
+        "low": 0.1,
+        "high": 0.95
+      },
+      {
+        "field": "state",
+        "cleaners": [
+          "no.priv.garshol.duke.cleaners.LowerCaseNormalizeCleaner"
+        ],
+        "comparator": "no.priv.garshol.duke.comparators.Levenshtein",
+        "low": 0.1,
+        "high": 0.95
+      },
+      {
+        "field": "population",
+        "cleaners": [
+          "no.priv.garshol.duke.cleaners.DigitsOnlyCleaner"
+        ],
+        "comparator": "no.priv.garshol.duke.comparators.NumericComparator",
+        "low": 0.1,
+        "high": 0.95
+      }
+    ]
+  }
+}'
+echo
+curl -s "localhost:9200/test/_search?pretty=true" -d '{
+  "size": 4,
+  "query": {
+    "custom_score": {
+      "query": {
+        "match_all": {
+          
+        }
+      },
+      "script": "entity-resolution",
+      "lang": "native",
+      "params": {
+        "entity": {
+          "configuration": {
+            "index": "entity",
+            "type": "entity-configuration",
+            "name": "test"
+          },
+          "fields": [
+            {
+              "field": "city",
+              "value": "South"
+            },
+            {
+              "field": "state",
+              "value": "ME"
+            },
+            {
+              "field": "population",
+              "value": "26000"
+            }
+          ]
+        }
+      }
+    }
+  }
+}'
